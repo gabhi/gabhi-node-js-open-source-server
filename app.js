87b09a320990974f29a3ede7d72a6cd2ba2a140c
Abhijit Gaikwad <gaikwad.abhijit@gmail.com>
2014-06-11 00:37:40 -0700

    Added CORS support i.e. enabled cross-origin resource sharing

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var config = require('nconf').argv().env().file({
     file: './config/app_config.json'
 });
 var PROFILER = config.get("PROFILER");
+var extras = require('express-extras');
 
 require('look').start(PROFILER.PORT, PROFILER.HOST);
 // Include the cluster module
@@ -39,6 +40,27 @@ if (cluster.isMaster && cluster_mode_enabled) {
     // Code to run if we're in a worker process
 } else {
     var app = express();
+
+    //Enable CORS
+    app.all('*', function(req, res, next) {
+        if (!req.get('Origin')) return next();
+        var ACCESS_CONTROL = config.get("ACCESS_CONTROL");
+        // use "*" here to accept any origin
+        res.set('Access-Control-Allow-Origin', ACCESS_CONTROL.ALLOW_ORIGIN);
+        res.set('Access-Control-Allow-Methods', ACCESS_CONTROL.ALLOW_METHODS);
+        res.set('Access-Control-Allow-Headers', ACCESS_CONTROL.ALLOW_HEADERS);
+
+        next();
+    });
+    process.env.NODE_ENV = config.get("NODE_ENV");
+    app.use(extras.throttle());
+    app.use(extras.throttle(config.get("THROTTLESETTINGS")));
+
+
+
+
+
+
     require("./routes")(app);
 
     // Bind to a port
